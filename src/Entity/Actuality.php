8ace0f84e3ba2427fbdbbea31a1f3316ff00c3d2
Thibault <thibaultfoure@gmail.com>
2022-02-02 16:13:43 +0100

    created entity actuality

---
+++ src/Entity/Actuality.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActualityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ActualityRepository::class)
+ */
+class Actuality
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private ?string $content;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private ?\DateTimeImmutable $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
