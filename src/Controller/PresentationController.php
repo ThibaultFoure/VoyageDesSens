cbf199e8e0789a8459dc25ac07c443baf4defa9d
Thibault <thibaultfoure@gmail.com>
2022-04-21 20:26:13 +0200

    navbar and footer ok

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/parcours", name: "journey")]
-class JourneyController extends AbstractController
+#[Route("/parcours", name: "presentation")]
+class PresentationController extends AbstractController
 {
 
     #[Route("/", name: "")]
     public function index(): Response
     {
-        return $this->render('journey/index.html.twig');
+        return $this->render('presentation/index.html.twig');
     }
 }
