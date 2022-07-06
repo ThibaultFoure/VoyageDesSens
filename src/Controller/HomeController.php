c0faed35c16f50c98ff09f882db626c087260b3e
Thibault <thibaultfoure@gmail.com>
2022-07-06 13:50:35 +0200

    fix new routes name for home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route("/", name: "home")]
+#[Route("/", name: "home_")]
 class HomeController extends AbstractController
 {
 
-    #[Route("/", name: "")]
+    #[Route("/", name: "index")]
     public function index(ActualityRepository $actualityRepository): Response
     {
         $actualities = $actualityRepository->findBy([], ['createdAt' => 'DESC'], 3);
         return $this->render('home/index.html.twig', ['actualities' => $actualities]);
+    }
+
+    #[Route("/mentions-legales", name: "legal_notice")]
+    public function legalNoticeIndex(): Response
+    {
+        return $this->render('legal_notice/index.html.twig');
     }
 }
